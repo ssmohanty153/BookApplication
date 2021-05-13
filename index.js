f7b22e89b52fc191e58de4efa24166bd0c46318d
ssmohant153 <ssmohanty153@gmail.com>
2021-05-13 16:34:47 +0530

    update for html

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ app.use(cors());
 app.get('/', (req, res) => {
     res.send('Hello World subhransu');
 });
+app.get('/book/:isbn', (req, res) => {
+    // Reading isbn from the URL
+    const isbn = req.params.isbn;
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -22,6 +26,29 @@ app.post('/book', (req, res) => {
     console.log(book);
     books.push(book);
     res.send('Book is added to the database');
+});
+app.delete('/book/:isbn', (req, res) => {
+    const isbn = req.params.isbn;
+    books = books.filter(i => {
+        if (i.isbn !== isbn) {
+            return true;
+        }
+        return false;
+    });
+
+    res.send('Book is deleted');
+});
+app.post('/book/:isbn', (req, res) => {
+    const isbn = req.params.isbn;
+    const newBook = req.body;
+    for (let i = 0; i < books.length; i++) {
+        let book = books[i]
+        if (book.isbn === isbn) {
+            books[i] = newBook;
+        }
+    }
+
+    res.send('Book is edited');
 });
 
 app.listen(port, () => console.log(`Book app listening on port ${port}!`))
