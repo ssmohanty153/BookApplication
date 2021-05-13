a535dd394d364a59af82653d15aee7bcfd768611
ssmohant153 <ssmohanty153@gmail.com>
2021-05-13 13:08:36 +0530

    Update for html

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
-const express =require("express");
-const app=express();
-const port=3000;
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+const port = 3000;
+let books = [];
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello World subhransu');
 });
-app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+app.get('/books', (req, res) => {
+    res.json(books);
+})
+app.post('/book', (req, res) => {
+    const book = req.body;
+    console.log(book);
+    books.push(book);
+    res.send('Book is added to the database');
+});
+
+app.listen(port, () => console.log(`Book app listening on port ${port}!`))
